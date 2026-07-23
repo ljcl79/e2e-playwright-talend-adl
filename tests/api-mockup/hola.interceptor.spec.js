e53bb622efd1be20600398461b20f9c4110b411a
ljcl79 <ljcl79@gmail.com>
2026-07-22 21:30:33 -0300

    Feat: Mockup API

---
+++ tests/api-mockup/hola.interceptor.spec.js
@@ -0,0 +1,27 @@
+import test from "@playwright/test";
+
+test.describe("Probando Escenarios de Backend", () => {
+    test("Interceptando imagenes", async ({ page }) => {
+        await page.route('**\/*.{png,jpg,jpeg}', route => route.abort());
+
+        await page.goto('https://ecommerce-js-test.vercel.app/');
+        await page.getByRole('textbox', { name: 'Search products...' }).fill('mens');
+
+        //Criterios de aceptación
+
+    });
+
+    test("Actuando ante error del backend", async ({ page }) => {
+        await page.route('**/products', async (route, request) => {
+            //await new Promise(resolve => setTimeout(resolve, 10000));
+            await route.fulfill({
+                status: 200,
+                body: JSON.stringify([])
+            });
+            await route.fulfill
+        });
+        await page.goto('https://ecommerce-js-test.vercel.app/');
+        await page.getByRole('textbox', { name: 'Search products...' }).fill('mens');
+
+    })
+});
